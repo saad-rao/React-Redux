6a5bbb6e7ed3e6818502919d65bf4aeea8fedc1e
Saad_Naseem <raosad786@gmail.com>
2025-05-20 04:01:38 +0500

    Add redux-persist for state persistence and update store configuration

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import { store } from './store/store.js'
+import { persistor, store } from './store/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
 <Provider store={store}>
-    <App />
+  <PersistGate loading={null} persistor={persistor}>
+      <App />
+
+
+      </PersistGate>
+
+    
     </Provider>
 
 )
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,20 @@
 import {createStore} from "redux";
 import { CounterReducer } from "./reducers/CounterReducer";
+import storage from 'redux-persist/lib/storage'
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+ 
+const persistedReducer = persistReducer(persistConfig, CounterReducer)
+
+ export let store = createStore(persistedReducer)
+ export let persistor = persistStore(store)
 
 
-export const store = createStore(CounterReducer)
+
 
 console.log(store.getState());
