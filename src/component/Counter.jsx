c8014084386adb86ce773f0e1a54bad3b28f669f
Saad_Naseem <raosad786@gmail.com>
2025-05-20 03:37:28 +0500

    Add Redux for state management

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Counter = () => {
+
+  const dispatch = useDispatch()
+  const data = useSelector((state)=>state)
+  console.log(data);
+  
   return (
     <>
    {/* <div>Counter</div> */}
     <div className="flex justify-center items-center h-screen bg-amber-200">
         <div className="bg-gray-200 p-4 rounded-lg shadow-lg">
           <h1 className="text-2xl font-bold mb-4 text-center">Counter</h1>
-          <p className="text-xl text-center">Count: 0</p>
-          <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+          <p className="text-xl text-center">Count: {data.count}</p>
+          <button onClick={()=>dispatch({type:"inc"})} className="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
              Increment
           </button>
-           <button className="mt-4 bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 ml-5">
+           <button onClick={()=>dispatch({type:"dec"})} className="mt-4 bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 ml-5">
              Decrement
           </button>
         </div>
